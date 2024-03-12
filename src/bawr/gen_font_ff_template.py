f19b08647fd3816282eb73dfcbb2abe29e5edb76
schoenemann <67146370+schoenemann@users.noreply.github.com>
2024-03-12 23:00:26 +0100

    Add ligature lookups to the font file (#2)
    * write empty latin characters needed for ligatures
    
    All glyphes that should be replaced by a ligatures have to be included
    in the font. This is a subset of latin characters we use to create
    ligatures from
    
    * create lookup tables and add every glyph
    
    Create a lookup table for latin ligature substitution.
    The glyph name is used as ligature so and we only use the
    characters [a-zA-Z0-9_] to create ligatures. That means
    that only these characters should appear in glyph names
    or else the lookup table will not work (i.e. no ligatures at all)

--- src/bawr/gen_font_ff_template.py
+++ src/bawr/gen_font_ff_template.py
@@ -68,9 +68,107 @@ font.fontname = config.font_name
 font.fullname = config.font_name
 font.version = config.font_version.strip() or font.version
 
+# add latin characters to be used in
+ligaChars = [
+      (0x30, 'zero'),
+      (0x31, 'one'),
+      (0x32, 'two'),
+      (0x33, 'three'),
+      (0x34, 'four'),
+      (0x35, 'five'),
+      (0x36, 'six'),
+      (0x37, 'seven'),
+      (0x38, 'eight'),
+      (0x39, 'nine'),
+
+      (0x41, 'A'),
+      (0x42, 'B'),
+      (0x43, 'C'),
+      (0x44, 'D'),
+      (0x45, 'E'),
+      (0x46, 'F'),
+      (0x47, 'G'),
+      (0x48, 'H'),
+      (0x49, 'I'),
+      (0x4a, 'J'),
+      (0x4b, 'K'),
+      (0x4c, 'L'),
+      (0x4d, 'M'),
+      (0x4e, 'N'),
+      (0x4f, 'O'),
+      (0x50, 'P'),
+      (0x51, 'Q'),
+      (0x52, 'R'),
+      (0x53, 'S'),
+      (0x54, 'T'),
+      (0x55, 'U'),
+      (0x56, 'V'),
+      (0x57, 'W'),
+      (0x58, 'X'),
+      (0x59, 'Y'),
+      (0x5a, 'Z'),
+
+      (0x5f, 'underscore'),
+
+      (0x61, 'a'),
+      (0x62, 'b'),
+      (0x63, 'c'),
+      (0x64, 'd'),
+      (0x65, 'e'),
+      (0x66, 'f'),
+      (0x67, 'g'),
+      (0x68, 'h'),
+      (0x69, 'i'),
+      (0x6a, 'j'),
+      (0x6b, 'k'),
+      (0x6c, 'l'),
+      (0x6d, 'm'),
+      (0x6e, 'n'),
+      (0x6f, 'o'),
+      (0x70, 'p'),
+      (0x71, 'q'),
+      (0x72, 'r'),
+      (0x73, 's'),
+      (0x74, 't'),
+      (0x75, 'u'),
+      (0x76, 'v'),
+      (0x77, 'w'),
+      (0x78, 'x'),
+      (0x79, 'y'),
+      (0x7a, 'z'),
+]
+
+def mapCharToName(c):
+    return {
+        '0': 'zero',
+        '1': 'one',
+        '2': 'two',
+        '3': 'three',
+        '4': 'four',
+        '5': 'five',
+        '6': 'six',
+        '7': 'seven',
+        '8': 'eight',
+        '9': 'nine',
+        '_': 'underscore',
+    }.get(c, c)
+
+for code, name in ligaChars:
+    glyph = font.createChar(code, name)
+    pen = glyph.glyphPen()
+    pen.moveTo(0,0)
+
+lookup_name = '\'liga\' Standard Ligatures in Latin lookup 0'
+subtable_name = lookup_name + ' subtable'
+font.addLookup( lookup_name, "gsub_ligature", [], [ ("liga",[("latn",["dflt"])]) ] )
+font.addLookupSubtable(lookup_name, subtable_name)
+
 for code, name, path, options in select:
     glyph = font.createChar(code, name)
     glyph.importOutlines(path)
+
+    ligature_components = ' '.join( map(mapCharToName, list(name)) )
+    glyph.addPosSub(subtable_name, ligature_components)
     if options:
         lt = parse_transformation(options.get('font_transformation', None)) 
         if lt:
